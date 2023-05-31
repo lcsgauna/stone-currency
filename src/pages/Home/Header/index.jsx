d8e1747aaa513d38903934c8ad655c7eddbe4e25
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2023-05-30 23:43:08 -0400

    feat: Adicionando background e iniciando form

--- src/pages/Home/Header/index.jsx
+++ src/pages/Home/Header/index.jsx
@@ -4,15 +4,49 @@ import styles from './Header.module.css'
 function Header(){
     const ls_cotacao = JSON.parse(localStorage.getItem('@cotacao'))
     const cotacao_info = ls_cotacao['USDBRL']
+    const data_cotacao = new Date(cotacao_info['create_date']);
+
+    function obterNomeDoMes(numeroDoMes) {
+        switch (numeroDoMes) {
+          case 1:
+            return "Janeiro";
+          case 2:
+            return "Fevereiro";
+          case 3:
+            return "Março";
+          case 4:
+            return "Abril";
+          case 5:
+            return "Maio";
+          case 6:
+            return "Junho";
+          case 7:
+            return "Julho";
+          case 8:
+            return "Agosto";
+          case 9:
+            return "Setembro";
+          case 10:
+            return "Outubro";
+          case 11:
+            return "Novembro";
+          case 12:
+            return "Dezembro";
+          default:
+            return "Mês inválido";
+        }
+      }
 
     return(
         <>
-            <a href="">
-                <img src={Logo} alt="Stone currency"/>
-            </a>
-            <div className='api-info'>
-                <p className={styles.apiInfoTitle}>{cotacao_info['create_date']}</p>
-                <p className={styles.apiInfoSubtitle}>Dados de câmbio disponibilizados pela Awesomeapi.</p>
+            <div className={styles.headerContainer}>
+                <a href="">
+                    <img src={Logo} alt="Stone currency"/>
+                </a>
+                <div className='api-info'>
+                    <p className={styles.apiInfoTitle}>{`${data_cotacao.getDay()} de ${obterNomeDoMes(data_cotacao.getMonth())} ${data_cotacao.getFullYear()} |  ${data_cotacao.getHours()}:${data_cotacao.getMinutes()}${data_cotacao.getSeconds()} UTC`}</p>
+                    <p className={styles.apiInfoSubtitle}>Dados de câmbio disponibilizados pela Awesomeapi.</p>
+                </div>
             </div>
         </>
     )
--- src/pages/Home/Section/index.jsx
+++ src/pages/Home/Section/index.jsx
@@ -1,6 +1,23 @@
+import styles from './Section.module.css'
 function Section(){
     return(
         <>
+        <div className={styles.currencyCard}>
+            <form className={styles.currencyCardForm}>
+                <label>Dólar</label>
+                <label>Taxa do Estado</label>
+                <input placeholder='$'/>
+                <input placeholder='0%'/>
+
+                <label>Tipo de Compra</label>
+                <div className={styles.currencyCardInputRadio}>
+                <input type='radio' value='dinheiro'/>
+                <label>Dinheiro</label>
+                <input type='radio' value='cartao'/>
+                <label>Cartão</label>
+                </div>
+            </form>
+        </div>
         </>
     )
 }
